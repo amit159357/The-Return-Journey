6c1568afbde398afad9129bf2c533f0a3bcd9b8a
amit159357 <bhandariamit108@gmail.com>
2024-09-19 20:30:26 +0530

    Meta tags changed

--- src/index.js
+++ src/index.js
@@ -9,14 +9,10 @@ import store from "./app/store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-
-      <Provider store={store}>
-     
-        <App />
-      </Provider>
-    
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
-
 
 reportWebVitals();
